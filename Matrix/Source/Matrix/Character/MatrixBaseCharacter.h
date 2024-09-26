1a7af95c4fab06b29f9bc478f55bfd04f05aa0a6
JUNSUNG06 <forest2847@gmail.com>
2024-09-26 12:43:35 +0900

    setup project

---
+++ Matrix/Source/Matrix/Character/MatrixBaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MatrixCharacter.h"
+#include "MatrixBaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATRIX_API AMatrixBaseCharacter : public AMatrixCharacter
+{
+	GENERATED_BODY()
+	
+};

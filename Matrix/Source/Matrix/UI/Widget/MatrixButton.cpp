9fe8ad22553f15b7ec71bcf1031fb2ce9b025d7f
JUNSUNG06 <forest2847@gmail.com>
2024-10-21 22:11:34 +0900

    .

--- Matrix/Source/Matrix/UI/Widget/MatrixButton.cpp
+++ Matrix/Source/Matrix/UI/Widget/MatrixButton.cpp
@@ -9,12 +9,25 @@ UMatrixButton::UMatrixButton()
 	OnHovered.AddDynamic(this, &UMatrixButton::OnHover);
 	OnUnhovered.AddDynamic(this, &UMatrixButton::OnUnhover);
 	OnClicked.AddDynamic(this, &UMatrixButton::OnClick);
+
+	OnHoverColor = FLinearColor(1.0f, 0.03f, 0.07f, 1.0f);
+	DefaultColor = FLinearColor::White;
+
+	ConstructorHelpers::FObjectFinder<USoundBase> ClickSoundRef(
+		TEXT("/Game/Matrix/ETC/Sound/Cue/UI/SU_Button_Click.SU_Button_Click"));
+	if (ClickSoundRef.Succeeded())
+		OnClickSound = ClickSoundRef.Object;
+	ConstructorHelpers::FObjectFinder<USoundBase> HoverSoundRef(
+		TEXT("/Game/Matrix/ETC/Sound/Cue/UI/SU_Button_Hover.SU_Button_Hover"));
+	if (HoverSoundRef.Succeeded())
+		OnHoverSound = HoverSoundRef.Object;
 }
 
 void UMatrixButton::OnHover()
 {
 	SetColorAndOpacity(OnHoverColor);
-	UGameplayStatics::PlaySound2D(GetWorld(), OnHoverSound);
+	if(OnHoverSound)
+		UGameplayStatics::PlaySound2D(GetWorld(), OnHoverSound);
 }
 
 void UMatrixButton::OnUnhover()
@@ -24,5 +37,6 @@ void UMatrixButton::OnUnhover()
 
 void UMatrixButton::OnClick()
 {
-	UGameplayStatics::PlaySound2D(GetWorld(), OnClickSound);
+	if(OnClickSound)
+		UGameplayStatics::PlaySound2D(GetWorld(), OnClickSound);
 }
